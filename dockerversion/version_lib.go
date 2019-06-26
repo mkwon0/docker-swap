65139f20bb3cac7af288a19cb8f945dc8a805ff8
mkwon <mkwon@camelab.org>
2019-06-26 15:54:26 +0900

    init

---
+++ dockerversion/version_lib.go
@@ -0,0 +1,16 @@
+// +build !autogen
+
+// Package dockerversion is auto-generated at build-time
+package dockerversion
+
+// Default build-time variable for library-import.
+// This file is overridden on build with build-time informations.
+const (
+	GitCommit          string = "89658be"
+	Version            string = "17.05.0-ce"
+	BuildTime          string = "2019-06-24T15:08:14.233556711+09:00"
+	IAmStatic          string = "library-import"
+	ContainerdCommitID string = "library-import"
+	RuncCommitID       string = "library-import"
+	InitCommitID       string = "library-import"
+)
